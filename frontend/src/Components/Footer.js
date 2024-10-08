9038832ef9e9d39e5e850ea66743ca588aee4d07
padhara-meet0069 <153054729+padhara-meet0069@users.noreply.github.com>
2024-10-08 12:49:32 +0530

    Add files via upload

---
+++ frontend/src/Components/Footer.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import './Footer.css'
+
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TelegramIcon from '@material-ui/icons/Telegram';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <div>
+                <div className='footer-data'>
+                    <div className="contact-details">
+                        <h1>Contact Us</h1>
+                        <p>Padhara Meet</p>
+                        <p>Web Developer</p>
+                        <p>Ahmedabad</p>
+                        <p>Gujrat</p>
+                        <p>India</p>
+                        <p><b>Email:</b>meet@gmail.com</p>
+                    </div>
+                    <div className='usefull-links'>
+                        <h1>Usefull Links</h1>
+                        <a href='#home'>Link-1</a>
+                        <a href='#home'>Link-1</a>
+                        <a href='#home'>Link-1</a>
+                        <a href='#home'>Link-1</a>
+                    </div>
+                    <div className='librarian-details'>
+                        <h1>Info.</h1>
+                        <p>Meet</p>
+                        <p>Padhara</p>
+                        <p>Contact: +91 1234567890</p>
+                    </div>
+                </div>
+                <div className="contact-social" >
+                    <a href='#home' className='social-icon'><TwitterIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
+                    <a href='#home' className='social-icon'><LinkedInIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
+                    <a href='#home' className='social-icon'><TelegramIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
+                    <a href='#home' className='social-icon'><InstagramIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
+                </div>
+            </div>
+            <div className='copyright-details'>
+                <p className='footer-copyright'>&#169; 2024 copyright all right reserved<br /><span>Padhara meet</span></p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
